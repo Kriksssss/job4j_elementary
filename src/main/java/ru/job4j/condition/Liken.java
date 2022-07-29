c78a322513f560c804556c19bceb901120aa54ba
Kriksssss <nabibeg53@gmail.com>
2022-07-29 23:21:09 +0300

    Операции сравнения [#235631 #326980]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -4,6 +4,11 @@ public class Liken {
     public static void main(String[] args) {
         int first = 10;
         int second = 9;
-        System.out.println(first + second);
+        boolean result = first > second;
+        System.out.println(result);
+        result = first < second;
+        System.out.println(result);
+        result = first == second;
+        System.out.println(result);
     }
 }
