ee445fb7ef5baec4ecd0c55a6de3b7288bf4e619
Kriksssss <nabibeg53@gmail.com>
2022-09-03 05:31:15 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -13,6 +13,10 @@ public class ArrayDefinition {
         names[1] = "Arsentev";
         names[2] = "Petr";
         names[3] = "Nabi";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
     }
 }
